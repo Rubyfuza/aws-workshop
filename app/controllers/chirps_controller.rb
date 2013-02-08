ca55671207ae4466220e9fa67d0363b1bcfc9c7d
Matthew Eddey <matt.eddey@gmail.com>
2013-02-08 15:33:07 +0200

    Tidied up chirps controller, added _chirp partial

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -2,39 +2,12 @@ class ChirpsController < ApplicationController
   # GET /chirps
   # GET /chirps.json
   def index
-    @chirps = Chirp.all
+    @chirps = Chirp.all.reverse
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @chirps }
     end
-  end
-
-  # GET /chirps/1
-  # GET /chirps/1.json
-  def show
-    @chirp = Chirp.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @chirp }
-    end
-  end
-
-  # GET /chirps/new
-  # GET /chirps/new.json
-  def new
-    @chirp = Chirp.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @chirp }
-    end
-  end
-
-  # GET /chirps/1/edit
-  def edit
-    @chirp = Chirp.find(params[:id])
   end
 
   # POST /chirps
@@ -44,40 +17,12 @@ class ChirpsController < ApplicationController
 
     respond_to do |format|
       if @chirp.save
-        format.html { redirect_to @chirp, notice: 'Chirp was successfully created.' }
+        format.html { redirect_to '/', notice: 'Chirp was successfully created.' }
         format.json { render json: @chirp, status: :created, location: @chirp }
       else
         format.html { render action: "new" }
         format.json { render json: @chirp.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PUT /chirps/1
-  # PUT /chirps/1.json
-  def update
-    @chirp = Chirp.find(params[:id])
-
-    respond_to do |format|
-      if @chirp.update_attributes(params[:chirp])
-        format.html { redirect_to @chirp, notice: 'Chirp was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @chirp.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /chirps/1
-  # DELETE /chirps/1.json
-  def destroy
-    @chirp = Chirp.find(params[:id])
-    @chirp.destroy
-
-    respond_to do |format|
-      format.html { redirect_to chirps_url }
-      format.json { head :no_content }
     end
   end
 
