4626ed14c77cfd59cb7ea3f4433bdb0b75724b1d
Marc Bowes <bowes@amazon.com>
2013-02-08 16:13:34 +0200

    Remove tests for chirps controller that reference non-existent actions

--- test/functional/chirps_controller_test.rb
+++ test/functional/chirps_controller_test.rb
@@ -11,39 +11,11 @@ class ChirpsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:chirps)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create chirp" do
     assert_difference('Chirp.count') do
       post :create, chirp: { content: @chirp.content, user: @chirp.user }
     end
 
     assert_redirected_to chirp_path(assigns(:chirp))
-  end
-
-  test "should show chirp" do
-    get :show, id: @chirp
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @chirp
-    assert_response :success
-  end
-
-  test "should update chirp" do
-    put :update, id: @chirp, chirp: { content: @chirp.content, user: @chirp.user }
-    assert_redirected_to chirp_path(assigns(:chirp))
-  end
-
-  test "should destroy chirp" do
-    assert_difference('Chirp.count', -1) do
-      delete :destroy, id: @chirp
-    end
-
-    assert_redirected_to chirps_path
   end
 end
